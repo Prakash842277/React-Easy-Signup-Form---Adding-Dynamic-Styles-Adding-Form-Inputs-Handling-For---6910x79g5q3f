f8c0d551a56eda7d0dede73db26c23e193e68778
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-02-24 22:36:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,62 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useReducer, useState } from "react";
+import "../styles/App.css";
+import { signUpFormValidation } from "../utils/validation";
 
+const signUpFormReducer = (state, action) => {
+  switch (action.type) {
+    case "input":
+      return {
+        ...state,
+        input: {
+          ...state.input,
+          [action.payload.field]: action.payload.value
+        }
+      };
+    case "error":
+      return {
+        ...state,
+        errors: {
+          ...state.errors,
+          [action.payload.field]: action.payload.value
+        }
+      };
+    case "resetError":
+      const errors = {};
+      for (const t in state.errors) {
+        errors[t] = "";
+      }
+      return {
+        ...state,
+        errors
+      };
+    case "toggleConsent":
+      return {
+        ...state,
+        consent: !state.consent
+      };
+    default:
+      break;
+  }
+};
+
+const initialFormState = {
+  input: {
+    name: "",
+    email: "",
+    password: "",
+    consent: false
+  },
+  errors: {
+    name: "",
+    email: "",
+    password: ""
+  }
+};
 
 const App = () => {
-  
-  return (
+  const [formState, dispatch] = useReducer();
 
-  )
-}
-
+  return <></>;
+};
 
 export default App;
