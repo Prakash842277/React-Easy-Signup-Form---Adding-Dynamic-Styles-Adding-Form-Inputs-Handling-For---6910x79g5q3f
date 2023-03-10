7f216c8e8117f877f7fc934a315f05eeac38283f
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-03-10 13:23:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@
 
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
